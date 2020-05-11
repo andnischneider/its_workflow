04d807a1a25e9c9f3743d925462da83debb371f0
johnne <john.sundh@scilifelab.se>
2020-05-11 22:38:38 +0200

    Update sample dictionary

--- src/parse_samples.py
+++ src/parse_samples.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
 import pandas as pd
+from os.path import join as opj
 
 
-def get_samples(f, pool=False):
+def get_samples(f, config):
     samples = {}
+    pools = {}
     df = pd.read_csv(f, sep="\t", header=0)
-    genes = list(df.geneType.unique())
     for i in df.index:
         r = df.loc[i]
-        if pool:
-            runID = r.sampleRun
-            sampleName = r.sampleName
+        run_id = r.run_id
+        pool_id = r.sample_name
+        sample_id = "{}.{}".format(pool_id, run_id)
+        if "url" in df.columns:
+            source = r["url"]
+            config["source"] = "url"
+        elif "sra_id" in df.columns:
+            source = r["sra_id"]
+            config["source"] = "sra"
         else:
-            runID = 1
-            sampleName = "{}.{}".format(r.sampleName,r.sampleRun)
-        try:
-            acc = r.accession
-        except KeyError:
-            acc = sampleName
-        try:
-            samples[sampleName][runID] = {r.geneType: acc}
-        except KeyError:
-            samples[sampleName] = {runID: {r.geneType: acc}}
-    return samples, genes
+            source = ""
+        samples[sample_id] = source
+        if pool_id not in pools.keys():
+            pools[pool_id] = {"R1": [], "R2": []}
+        pools[pool_id]["R1"].append(opj(config["data_dir"],
+                                        "{}_1.fastq.gz".format(sample_id)))
+        pools[pool_id]["R2"].append(opj(config["data_dir"],
+                                        "{}_2.fastq.gz".format(sample_id)))
+    return samples, pools, config
