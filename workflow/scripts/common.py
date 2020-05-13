2feb6b2ae2316e0503e8beaa9acf6ab95a2dcf18
johnne <john.sundh@scilifelab.se>
2020-05-13 22:07:46 +0200

    Move sample parsing to common.py

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -1,13 +1,31 @@
-configfile: "config.yml"
 from os.path import join as opj
-from src.parse_samples import get_samples
 
-wildcard_constraints:
-    run = "\d+"
 
-# Use only a small set of reads if testing the workflow
-if config["test"]:
-    config["maxSpotId"] = "-X 1000"
-else:
-    config["maxSpotId"] = ""
-samples, pools, config = get_samples(config["sample_file"], config)
+def get_samples(df, data_dir):
+    """
+    Parses the sample list and generates a sample dictionary and a pool
+    dictionary with information on how the workflow should handle input.
+
+    :param df: pandas DataFrame with samples
+    :param data_dir: Input data directory from config
+    :return: sample_dict, pool_dict dictionaries
+    """
+    sample_dict = {}
+    pool_dict = {}
+    for i in df.index:
+        r = df.loc[i]
+        pool_id = r["sample"]
+        try:
+            run_id = r.run_id
+            sample_id = "{}.{}".format(pool_id, run_id)
+        except AttributeError:
+            sample_id = r.sample
+        if "sra_id" in df.columns:
+            sample_dict[sample_id] = r.sra_id
+        if pool_id not in pool_dict.keys():
+            pool_dict[pool_id] = {"R1": [], "R2": []}
+        pool_dict[pool_id]["R1"].append(
+            opj(data_dir, "{}_R1.fastq.gz".format(sample_id)))
+        pool_dict[pool_id]["R2"].append(
+            opj(data_dir, "{}_R2.fastq.gz".format(sample_id)))
+    return sample_dict, pool_dict
