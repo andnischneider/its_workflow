2deac7a22dabc714becb2080cb4597645883f308
johnne <john.sundh@scilifelab.se>
2020-05-13 11:17:30 +0200

    Autogenerate run id

--- src/parse_samples.py
+++ src/parse_samples.py
@@ -10,7 +10,10 @@ def get_samples(f, config):
     df = pd.read_csv(f, sep="\t", header=0)
     for i in df.index:
         r = df.loc[i]
-        run_id = r.run_id
+        try:
+            run_id = r.run_id
+        except KeyError:
+            run_id = "1"
         pool_id = r.sample_name
         sample_id = "{}.{}".format(pool_id, run_id)
         if "url" in df.columns:
@@ -21,11 +24,12 @@ def get_samples(f, config):
             config["source"] = "sra"
         else:
             source = ""
+            config["source"] = ""
         samples[sample_id] = source
         if pool_id not in pools.keys():
             pools[pool_id] = {"R1": [], "R2": []}
         pools[pool_id]["R1"].append(opj(config["data_dir"],
-                                        "{}_1.fastq.gz".format(sample_id)))
+                                        "{}_R1.fastq.gz".format(sample_id)))
         pools[pool_id]["R2"].append(opj(config["data_dir"],
-                                        "{}_2.fastq.gz".format(sample_id)))
+                                        "{}_R2.fastq.gz".format(sample_id)))
     return samples, pools, config
