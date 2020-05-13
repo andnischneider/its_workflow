71c3d53eacd08e5b253a49685dd313936c1b3a75
johnne <john.sundh@scilifelab.se>
2020-05-13 13:38:50 +0200

    Use prefilter.R with snakemake script directive
    Here the script is updated to make use of the script directive in snakemake which passes input/output, parameters and resources as an object.

--- src/R/prefilter.R
+++ src/R/prefilter.R
@@ -1,17 +1,21 @@
 #!/usr/bin/env Rscript
 
-require(dada2)
+require(dada2, quietly = TRUE)
+# IO
+R1 <- snakemake@input$R1
+R2 <- snakemake@input$R2
+R1_o <- snakemake@output$R1
+R2_o <- snakemake@output$R2
+# Parameters
+maxN <- snakemake@params$maxN
+truncQ <- snakemake@params$truncQ
+truncLen <- snakemake@params$truncLen
+maxEE <- snakemake@params$maxEE
+rm_phix <- snakemake@params$rm_phix
+minLen <- snakemake@params$minLen
+# Performance
+threads <- snakemake@threads
 
-args = commandArgs(trailingOnly=TRUE)
-
-if (length(args) != 5) {
-  stop('usage: cutITS.R <input R1> <input R2> <output R1> <output R2> <threads>')
-}
-
-R1 <- args[1]
-R2 <- args[2]
-R1_o <- args[3]
-R2_o <- args[4]
-threads <- args[5]
-
-filterAndTrim(R1, R1_o, R2, R2_o, maxN = 0, multithread = TRUE)
+filterAndTrim(fwd = R1, filt = R1_o, rev = R2, filt.rev = R2_o,
+              maxN = maxN, truncQ = truncQ, maxEE = maxEE, rm.phix = rm_phix,
+              minLen = minLen, multithread = threads)
