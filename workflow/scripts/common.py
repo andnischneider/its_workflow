f72ec0d2e95f645425833aa779be2e1240f51046
johnne <john.sundh@scilifelab.se>
2020-05-14 07:29:59 +0200

    Fix sample name handling and check for presence of start files

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -1,5 +1,5 @@
-from os.path import join as opj
-
+from os.path import join as opj, exists
+import sys
 
 def get_samples(df, data_dir):
     """
@@ -19,13 +19,28 @@ def get_samples(df, data_dir):
             run_id = r.run_id
             sample_id = "{}.{}".format(pool_id, run_id)
         except AttributeError:
-            sample_id = r.sample
+            sample_id = r["sample"]
+        # Set start location for sample
+        R1 = opj(data_dir, "{}_R1.fastq.gz".format(sample_id))
+        R2 = opj(data_dir, "{}_R2.fastq.gz".format(sample_id))
+        # See if the sample has an SRA id that can be used to download it
         if "sra_id" in df.columns:
             sample_dict[sample_id] = r.sra_id
+        # If not, check that the starting file exists under data dir
+        else:
+            if not exists(R1) or not exists(R2):
+                sys.exit("""
+                
+###### WORKFLOW ERROR ######
+Sample {} has no SRA id and missing at least one of
+R1: {}
+R2: {}
+###### WORKFLOW ERROR ######
+                
+                """.format(sample_id, R1, R2))
+        # Add files to pool
         if pool_id not in pool_dict.keys():
             pool_dict[pool_id] = {"R1": [], "R2": []}
-        pool_dict[pool_id]["R1"].append(
-            opj(data_dir, "{}_R1.fastq.gz".format(sample_id)))
-        pool_dict[pool_id]["R2"].append(
-            opj(data_dir, "{}_R2.fastq.gz".format(sample_id)))
+        pool_dict[pool_id]["R1"].append(R1)
+        pool_dict[pool_id]["R2"].append(R2)
     return sample_dict, pool_dict
