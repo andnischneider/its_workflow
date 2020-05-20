3309c60d5bc191a7aef348354487d9107282f429
Andreas Schneider <andreas.schneider1@t-online.de>
2020-05-20 09:46:40 +0200

    Added read number assessment, with output as csv file

--- workflow/scripts/runDada2.R
+++ workflow/scripts/runDada2.R
@@ -61,4 +61,10 @@ saveRDS(mergers, here(paste0(out, "/mergers.rds")))
 saveRDS(seqtab, here(paste0(out, "/seqtab.rds")))
 saveRDS(seqtab.nc, here(paste0(out, "/seqtab_nc.rds")))
 
+#Track reads through pipeline
+getN <- function(x) sum(getUniques(x))
+track <- cbind(readRDS(here(paste0(out, "out.rds"))), sapply(dadas_f, getN), sapply(dadas_r, getN), rowSums(seqtab), rowSums(seqtab.nc))
+colnames(track_n_i) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
+write.csv(track, here(paste0(out, "/read_counts.csv")), quote = FALSE)
+
 
